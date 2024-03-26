1ac8390b042d016d41b62408f44b4f8076d7ce7e
Islam Hany <ihhg.developer@gmail.com>
2024-03-26 22:17:01 +0200

    12. Log stats

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+12-log_stats.py
+"""
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["logs"]
+collection = db["nginx"]
+
+total_logs = collection.count_documents({})
+print(f"{total_logs} logs")
+print("Methods:")
+
+for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    total_per_method = collection.count_documents({'method': method})
+    print(f"\tmethod {method}: {total_per_method}")
+
+total_status_check = collection.count_documents({
+    'method': 'GET',
+    'path': '/status'
+})
+print(f"{total_status_check} status check")
